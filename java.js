052b12dbb774622aeea50de2884476d7ccd62d96
V1perzin <jose.teles@maisunifacisa.com.br>
2024-11-24 19:52:53 -0300

    Adicionando atualização no main

---
+++ java.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    <h1>React renderizado na div app</h1>,
+    document.getElementById("app")
+)
+
+ReactDOM.render(
+    <i>Segundo elemento React renderizado</i>,
+    document.getElementById("app2")
+)
